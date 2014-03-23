34ce8a73baf352cdfac365fe6e58aee17f164543
Hack Reactor Students <accounts@hackreactor.com>
2014-03-23 14:13:16 -0700

    responding to GET requests with test message

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,19 +1,41 @@
+var getHelper = require("./get-helper");
 
 exports.handleRequest = function(request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
-
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = "text/plain";
-
-  response.writeHead(statusCode, headers);
-  response.end("Hello, World!");
+  var method = requestRoutes[request.method];
+  console.log(method);
+  method(request, response);
 };
 
-var defaultCorsHeaders = {
+
+var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "text/plain"
+};
+
+var handleGet = function(req, res) {
+  console.log(JSON.stringify((req.url.slice(2).split("&"))));
+  var statusCode = 200;
+
+  res.writeHead(statusCode, headers);
+  res.end(JSON.stringify({results: [{username: "Wilford Testington", text: "I am an English gentlemen", roomname:"4chan", createdAt: new Date()}] }));
+};
+
+var requestRoutes = {
+  "GET": handleGet,
+  "POST": function(req, res){
+    var statusCode = 201;
+
+    res.writeHead(statusCode, headers);
+    res.end("Hello, World!");
+  },
+  "OPTIONS": function(req, res) {
+    var status = 200;
+    res.writeHead(status, headers);
+    res.end();
+  },
 };
